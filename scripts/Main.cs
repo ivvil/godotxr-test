f9529baeb3cbf7e4debe0f968e19a9d1450478d6
Iván Villagrasa <ivvil412@gmail.com>
2024-12-15 13:38:25 +0100

    Add android build and dev assets

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -3,7 +3,25 @@ using System;
 
 public partial class Main : Node3D
 {
-	private XRInterface _xrInterface;
+    private XRInterface _xrInterface;
 
-	
+    public override void _Ready()
+    {
+        _xrInterface = XRServer.FindInterface("OpenXR");
+
+        if (_xrInterface != null && _xrInterface.IsInitialized())
+        {
+            GD.Print("OpenXR initialized successfully");
+
+            // Vsync off
+            DisplayServer.WindowSetVsyncMode(DisplayServer.VSyncMode.Disabled);
+
+            // HMD video output
+            GetViewport().UseXR = true;
+        }
+		else
+		{
+			GD.Print("Error during OpenXR initialization");
+		}
+    }
 }
